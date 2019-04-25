5b329210a21f093219b3bb8b790732bb4a90bebf
Tobey Simpson <tsimpson89@yahoo.com>
2019-04-25 16:26:21 -0500

    first commit

---
+++ src/components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- Footer Section start -->
+    <div class="copyright-section index-footer bg-light">
+      <p>2019 Copyright | All Rights Reserved</p>
+    </div>
+    <!-- Footer Section End -->
+    <!-- Tap on Top -->
+    <div class="tap-top">
+      <div>
+        <i class="fa fa-angle-double-up"></i>
+      </div>
+    </div>
+    <!-- Tap on Ends -->
+    <color/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
